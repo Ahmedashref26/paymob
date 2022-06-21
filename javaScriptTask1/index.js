1edfa123220627c45561812efd52235bd8457e1c
Ahemd-Ashref <ahmedashref26@gmail.com>
2022-06-21 12:05:04 +0200

    js task 1 solution

--- javaScriptTask1/index.js
+++ javaScriptTask1/index.js
@@ -1,10 +1,13 @@
 const solution = (A, B, X, Y) => {
+  // function to detect if point is in the circle of radius r or not
   const isInCircle = (x, y, center, r = 20) => {
     const left = Math.pow(center[0] - x, 2) + Math.pow(center[1] - y, 2);
     const right = Math.pow(r, 2);
 
     return left <= right ? true : false;
   };
+
+  // loop over the icons
   for (let i = 0; i < A.length; i++) {
     if (isInCircle(X, Y, [A[i], B[i]])) return i;
   }
